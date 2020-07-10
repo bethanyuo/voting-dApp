92913a091630b4c3f32ad9332c21a2c1b12b88d6
Beth <Beth@Bethanys-MBP.attlocal.net>
2020-07-10 10:45:20 -0500

    delete Web3 demos & resource

--- 01.web3-demo/01_example.js
+++
@@ -1,7 +0,0 @@
-let Web3 = require('web3')
-let web3 = new Web3("http://localhost:8545")
-
-web3.eth.getCoinbase().then(function(addr) {
-    console.log(addr)
-    web3.eth.getBalance(addr).then(console.log)
-})
